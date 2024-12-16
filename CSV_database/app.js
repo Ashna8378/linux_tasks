3d5c1242ba6402f4d4482c57a6439afbfe04373c
Ashna8378 <dubey123@gmail.com>
2024-12-16 12:02:30 +0530

    initial commit

---
+++ CSV_database/app.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+
+fs.readFile("database.csv",'utf8', (err, data) => {
+
+    if (err) {
+    
+    console.error(err);
+    
+    return;
+    
+    }
+    
+    console.log(data);
+    
+});
+
+
